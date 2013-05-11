1d17eda6d1bfe10aa2361fe1cab7c3f612dd9af7
Joel Pedraza <github@saik0.net>
2013-05-10 21:51:49 -0300

    Add map init hook
    positionControl: false in map options will add the control with the  default options

--- src/L.Control.MousePosition.js
+++ src/L.Control.MousePosition.js
@@ -31,7 +31,17 @@ L.Control.MousePosition = L.Control.extend({
   }
 
 });
+L.Map.mergeOptions({
+    positionControl: false
+});
+
+L.KSP.Map.addInitHook(function () {
+    if (this.options.positionControl) {
+        this.positionControl = new L.Control.MousePosition();
+        this.addControl(this.positionControl);
+    }
+});
 
 L.control.mousePosition = function (options) {
-  return new L.Control.MousePosition(options);
+    return new L.Control.MousePosition(options);
 };
